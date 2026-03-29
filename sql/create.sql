7b3c18b762f42ba13d4c1a3bed32ff5291d6de6a
Yohana Triana <ytaranzola@gmail.com>
2026-03-29 11:06:49 +0200

    Create create.sql

---
+++ sql/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE form(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email varchar(100) not null,
+  age numeric check(age >= 18),
+  subject varchar(100) not null,
+  message varchar(500) not null
+)
