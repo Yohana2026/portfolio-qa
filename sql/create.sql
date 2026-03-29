299f06e82de5bcd01dc475fd147c899f7e3823b3
Yohana Triana <ytaranzola@gmail.com>
2026-03-29 11:11:06 +0200

    Delete sql directory

--- sql/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE form(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  email varchar(100) not null,
-  age numeric check(age >= 18),
-  subject varchar(100) not null,
-  message varchar(500) not null
-)
