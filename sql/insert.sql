ebe55bcc332904ba6a193f84e441018876b91a09
Yohana Triana <ytaranzola@gmail.com>
2026-03-29 14:06:59 +0200

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,1 +1,12 @@
-
+INSERT into form(name, email, age, subject, message)
+values
+('Ana', 'ana@gmail.com', NULL, 'information',	'Hello testing'),
+(NULL, 'pedro@gmail.com',	20,	'job',	'pedro message'),
+('maria',	'maria@yahoo.com',	25,	'other',	'test message'),   
+('ana',	'ana.work@gmail.com',	30,	'job',	'test message'),
+(NULL,	'user1@gmail.com',	22,	'information', 'mensaje testing'),
+('pedro',	'pedro@test.com',	35,	'other',	'test mensaje largo'),
+('juan',	'juan@gmail.com',	NULL,	'job',	'Hello QA testing'),
+('ana',	'ana.test@mail.com',	28,	'information',	'message with test'),
+(NULL,	'contact@gmail.com',	40,	'job',	'test email gmail'),
+('laura',	'laura@mail.com',	19,	'other',	'final test message');                                     
